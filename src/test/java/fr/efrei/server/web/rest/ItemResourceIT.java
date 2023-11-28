67f8a31e2526d26a29fe4c1cc05890e9f27d85df
Pierre Filliolaud <pierre.filliolaud@ormex.io>
2023-11-28 23:25:50 +0100

    Add test

---
+++ src/test/java/fr/efrei/server/web/rest/ItemResourceIT.java
@@ -0,0 +1,31 @@
+package fr.efrei.server.web.rest;
+
+import fr.efrei.server.domain.Item;
+import fr.efrei.server.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
+public class ItemResourceIT {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    @Transactional
+    void createItem() throws Exception {
+        int databaseSizeBeforeCreate = itemRepository.findAll().size();
+        assertThat(databaseSizeBeforeCreate).isEqualTo(0);
+
+//        List<Item> itemList = itemRepository.findAll();
+//        assertThat(itemList).hasSize(databaseSizeBeforeCreate + 1);
+    }
+}
